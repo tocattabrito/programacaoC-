d6d6c960f1d1a191ae60b7fc69de0c22c1a8289a
tocattabrito <tocattabrito@gmail.com>
2023-11-30 20:22:51 -0300

    upadate do arquivo sobrecarga

--- sobrecarga_funcao/sobrecarga.cpp
+++ sobrecarga_funcao/sobrecarga.cpp
@@ -12,10 +12,23 @@ void mensagem()
     cout << "hello world" << endl;
 }
 
+int soma(int n1, int n2 )
+{
+    return n1 + n2;
+}
+
+int soma(int n1, int n2, int n3)
+{
+    return n1 + n2 + n3;
+}
+
 // Função Principal
 int main(int argc, char const *argv[])
 {
     mensagem(19);
     mensagem();
+
+    cout << soma(1, 2) << endl;
+    cout << soma(1, 2, 3) << endl;
     return 0;
 }
