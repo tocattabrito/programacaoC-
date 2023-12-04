802ddda6079d6f4fd93d5f40971958340389b5de
tocattabrito <tocattabrito@gmail.com>
2023-12-04 09:40:26 -0300

    Nota Fiscal

---
+++ nota_fiscal/notaFiscal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// funções
+
+void cabecalho ()
+{
+    cout << "Imãos Silva Supermecados Ltda " << endl;
+    cout << "CNPJ:  11.023.654 / 0001 - 09 " << endl;
+    cout << "IE: 028998758 FONE:(81) 3485-2356"<< endl;
+    cout << "Rua dos coqueiros, 367, Candeias,\nJABOATÃO DOS GUARARAPES, PERNAMBUCO \n" << endl;
+
+    cout << "--------------------------------------" << endl;
+    
+
+}
+
+int main(int argc, const char** argv) {
+
+    cabecalho();
+    return 0;
+}
