0bf2c3b87bb42298908e29d42f8bbc50732226c8
tocattabrito <tocattabrito@gmail.com>
2023-12-11 23:01:47 -0300

    Update

--- ponteiro/ponteiro.cpp
+++ ponteiro/ponteiro.cpp
@@ -9,5 +9,19 @@ int main(int argc, char const *argv[])
 
     pvar = &var; // iniciar ponteiro
     cout << *pvar << endl;
+
+
+    // Alocando espaço na memoria
+
+    int *vet = new int[10];
+
+    *(vet + 0) = 10;
+    *(vet + 1) = 20;
+    *(vet + 2) = 30;
+
+    cout << *(vet + 0) << endl;
+    cout << *(vet + 1) << endl;
+    cout << *(vet + 2) << endl;
+
     return 0;
 }
