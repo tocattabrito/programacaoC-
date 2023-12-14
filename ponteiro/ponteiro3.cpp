8fa1c36ecab50b93ff73655fda3263b741037b96
tocattabrito <tocattabrito@gmail.com>
2023-12-14 06:08:29 -0300

    Update

--- ponteiro/ponteiro3.cpp
+++ ponteiro/ponteiro3.cpp
@@ -5,17 +5,21 @@ using namespace std;
 
 int main() {
 
-    int auxiliar[4];
+    int *auxiliar = new int[4];
 
     cout << "Digite um número: ";
     cin >> *(auxiliar);
-    cout << "Você digitou: " << auxiliar[0] << endl;
+    cout << "Você digitou: " << *(auxiliar) << endl;
 
     cout << "\n";
 
     cout << "Digite outro número: ";
     cin >> *(auxiliar + 1);
-    cout << "Você digitou: " << auxiliar[1] << endl;
+    cout << "Você digitou: " << *(auxiliar + 1) << endl;
+
+    delete[] auxiliar; // liberar memoria
+
+    cout << "Você digitou: " << *(auxiliar) << endl;
 
     return 0;
 }
