9bdadf5b250bd4463bf27058fe4ebcf569f8db25
tocattabrito <tocattabrito@gmail.com>
2023-12-19 22:11:14 -0300

    Define

---
+++ funcoes/define.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#define MOSTRA cout << "Oi mundo";
+#define PI 3.1415
+
+int main(int argc, char const *argv[])
+{
+    MOSTRA
+    cout << "\nPI = " << PI << "\n";
+   
+    return 0;
+}
