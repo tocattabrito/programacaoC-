52492d9ccaf8a4c4fb2d469e0f6bf5ab5bd544ec
tocattabrito <tocattabrito@gmail.com>
2023-12-19 22:22:52 -0300

    Entrada de dados via teclado

---
+++ teclado/cin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    int dia;
+
+    cout << "Qual a sua idade? ";
+    cin >> idade;
+
+    cout << "\nQue dia é hoje? ";
+    cin >> dia;
+
+    cout << "\n\nSua idade: " << idade;
+    cout << "\nHoje é dia " << dia << ".\n";
+}
