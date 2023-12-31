d79b2f4123797b6e03db1b8d5d92e1c13bd8526a
tocattabrito <tocattabrito@gmail.com>
2023-12-31 10:07:36 -0300

    Programa que calcular os dias já vividos

---
+++ teclado/cin2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int idade;
+    cout << "Qual a sua idade? ";
+    cin >> idade;
+    cout << "\nVocê tem " << idade * 356 << " dias de idade!\n";
+    return 0;
+}
